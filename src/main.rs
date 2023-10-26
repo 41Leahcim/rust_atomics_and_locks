d39a4ff43167d10374f498f512c6104cd5521ea8
Michael Scholten <michael-scholten@hotmail.nl>
2023-10-26 14:18:52 +0200

    Second thread example

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
 use std::thread;
 
 fn main() {
-    let threads = (0..2).map(|_| thread::spawn(f)).collect::<Vec<_>>();
+    let numbers = [1, 2, 3];
 
-    println!("Hello from the main thread.");
-    threads
-        .into_iter()
-        .for_each(|thread| thread.join().unwrap());
-}
-
-fn f() {
-    let id = thread::current().id();
-    println!("Hello from another thread\nThis is my thread id: {id:?}");
+    thread::spawn(move || {
+        for number in numbers {
+            println!("{number}");
+        }
+    })
+    .join()
+    .unwrap();
 }
