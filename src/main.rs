8d0296d5adc0903a91611d06260d130e9d6020b5
Michael Scholten <michael-scholten@hotmail.nl>
2023-10-26 14:47:55 +0200

    Memory leak example

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use std::thread;
 
-// A static variable isn't owned by any thread.
-// It is owned by the entire program.
-static X: [i32; 3] = [1, 2, 3];
-
 fn main() {
-    // This compiles, as it only borrows a static variable.
-    thread::spawn(|| dbg!(&X));
-    thread::spawn(|| dbg!(&X));
+    // Leaking memory, means it will live forever.
+    // The box::leak returns a static mutable reference to that memory.
+    // Mutable references can implicitly be turned into shared references.
+    // Leaked memory will never be deallocated which can cause the program to use up all memory.
+    let x: &'static [i32; 3] = Box::leak(Box::new([1, 2, 3]));
+
+    // The memory living forever allows us to pass it to threads.
+    thread::spawn(move || dbg!(x));
+    thread::spawn(move || dbg!(x));
 }
