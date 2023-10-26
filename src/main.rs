4520ac0e38225056b6d3d6b56a330e6f915c1319
Michael Scholten <michael-scholten@hotmail.nl>
2023-10-26 12:45:29 +0200

    First thread example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::thread;
+
 fn main() {
-    println!("Hello, world!");
+    thread::spawn(f);
+    thread::spawn(f);
+
+    println!("Hello from the main thread.");
+}
+
+fn f() {
+    println!("Hello from another thread!");
+
+    let id = thread::current().id();
+    println!("This is my thread id: {id:?}");
 }
