61098a6fa9d7b6f3f492e2ae339a7886f28388ec
Michael Scholten <michael-scholten@hotmail.nl>
2023-10-27 12:52:39 +0200

    Arc example with shadowing

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 use std::{sync::Arc, thread};
 
 fn main() {
-    // Thread safe reference counter
     let a = Arc::new([1, 2, 3]);
-    let b = a.clone();
 
-    let t1 = thread::spawn(move || dbg!(b));
+    let t1 = thread::spawn({
+        // Create a clone of Arc a with the same name, shadowing the original
+        let a = a.clone();
+
+        // move the clone to the new thread
+        move || dbg!(a)
+    });
+
+    // The original a is still available
     dbg!(a);
     t1.join().unwrap();
 }
