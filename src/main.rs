cdf417ec8bec5f0a93c13920c76d50847bde858c
Michael Scholten <michael-scholten@hotmail.nl>
2023-10-28 13:27:24 +0200

    Send and Sync implementation example

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
-use std::{cell::RefCell, fmt::Debug};
+use std::{cell::Cell, marker::PhantomData};
 
-fn f<T>(v: &RefCell<Vec<T>>)
-where
-    i32: TryInto<T>,
-    <i32 as TryInto<T>>::Error: Debug,
-{
-    // Take a mutable reference to more easily add a new element
-    v.borrow_mut().push(1.try_into().unwrap());
+// Structs with Cells aren't Sync, but they are send
+struct _NotSyncIsSend {
+    _handle: i32,
+    _not_sync: PhantomData<Cell<()>>,
 }
 
-fn main() {
-    // Cells and RefCells can only be used within a single thread
-    let values = RefCell::new(vec![0_i32]);
-    f(&values);
-    println!("{:?}", values.take());
+// Structs with pointers are neither Sync nor Send
+struct OnlySyncAndSendIfImplemented {
+    _p: *mut i32,
 }
+
+// Send and Sync aren't safe to implement as it can't be checked by the compiler.
+// Variables that aren't Send can't be move across threads
+unsafe impl Send for OnlySyncAndSendIfImplemented {}
+unsafe impl Sync for OnlySyncAndSendIfImplemented {}
+
+fn main() {}
