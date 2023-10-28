ea66ae7772a4af4c6ca7335f157e554a729fe812
Michael Scholten <michael-scholten@hotmail.nl>
2023-10-28 12:04:27 +0200

    RefCell example

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
-use std::{cell::Cell, fmt::Debug};
+use std::{cell::RefCell, fmt::Debug};
 
-fn f<T>(v: &Cell<Vec<T>>)
+fn f<T>(v: &RefCell<Vec<T>>)
 where
     i32: TryInto<T>,
     <i32 as TryInto<T>>::Error: Debug,
 {
-    // The old vec is returned and replaced with an empty vec
-    let mut v2 = v.take();
-    v2.push(1.try_into().unwrap());
-    v.set(v2);
+    // Take a mutable reference to more easily add a new element
+    v.borrow_mut().push(1.try_into().unwrap());
 }
 
 fn main() {
-    let values = Cell::new(vec![0_i32]);
+    // Cells and RefCells can only be used within a single thread
+    let values = RefCell::new(vec![0_i32]);
     f(&values);
     println!("{:?}", values.take());
 }
