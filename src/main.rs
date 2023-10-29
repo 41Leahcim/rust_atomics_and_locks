09540ab8d1ef430c7cb1038ca8e39d51f6bfe9c5
Michael Scholten <michael-scholten@hotmail.nl>
2023-10-29 14:37:53 +0100

    Fetch and modify operations example

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,10 @@
-use std::{
-    sync::atomic::{AtomicU64, Ordering},
-    thread,
-};
-
-fn calculate_x() -> u64 {
-    // Randomly generate a value >= 1
-    rand::random::<u64>().max(1)
-}
-
-fn get_x() -> u64 {
-    // Initialize an atomic u64 to 0
-    static X: AtomicU64 = AtomicU64::new(0);
-
-    // Load the value
-    let mut x = X.load(Ordering::Relaxed);
-
-    // Recalculate the value for x, if it is 0
-    if x == 0 {
-        x = calculate_x();
-        X.store(x, Ordering::Relaxed);
-    }
-
-    // Return the value
-    x
-}
+use std::sync::atomic::{AtomicI32, Ordering};
 
 fn main() {
-    // Start 2 threads retrieving x
-    let t1 = thread::spawn(get_x);
-    let t2 = thread::spawn(get_x);
+    let a = AtomicI32::new(100); // a = 100
+    let b = a.fetch_add(23, Ordering::Relaxed); // a = 123, b = 100
+    let c = a.load(Ordering::Relaxed); // a = 123, c = 123
 
-    // Panic if they aren't equal, which happens sometimes
-    // This can be solved with std::sync::Once or std::sync::OnceLock
-    assert_eq!(t1.join().unwrap(), t2.join().unwrap());
-
-    // Print the value of x
-    println!("{}", get_x());
+    assert_eq!(b, 100);
+    assert_eq!(c, 123);
 }
