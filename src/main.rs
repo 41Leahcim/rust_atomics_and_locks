28b4e3838ad6a88ddb8a8b645cab7fba63c4cfc7
Michael Scholten <michael-scholten@hotmail.nl>
2023-10-29 14:02:42 +0100

    Progress reporting example

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,37 @@
 use std::{
-    sync::atomic::{AtomicBool, Ordering},
+    fmt::Debug,
+    sync::atomic::{AtomicUsize, Ordering},
     thread,
+    time::Duration,
 };
 
-fn some_work() {}
+fn process_item<T: Debug>(item: T) {
+    println!("{item:?}")
+}
 
 fn main() {
-    static STOP: AtomicBool = AtomicBool::new(false);
+    let num_done = AtomicUsize::new(0);
 
-    // Spawn a thread to do the work.
-    let background_thread = thread::spawn(|| {
-        while !STOP.load(Ordering::Relaxed) {
-            some_work();
+    thread::scope(|s| {
+        // A background thread to process all 100 times
+        s.spawn(|| {
+            for i in 0..100 {
+                // Assuming this takes some time
+                process_item(i);
+                num_done.store(i + 1, Ordering::Relaxed);
+            }
+        });
+
+        // The main thread shows status updates, every second.
+        loop {
+            let n = num_done.load(Ordering::Relaxed);
+            if n == 0 {
+                break;
+            }
+            println!("Working... {n}/100 done");
+            thread::sleep(Duration::from_secs(1));
         }
     });
 
-    // Use the main thread to listen for user input.
-    for line in std::io::stdin().lines() {
-        match line.unwrap().as_str() {
-            "help" => println!("Commands: help, stop"),
-            "stop" => break,
-            cmd => println!("Unknown command: {cmd:?}\nCommands: help, stop"),
-        }
-    }
-
-    // Inform the background thread, it needs to stop!
-    STOP.store(true, Ordering::Relaxed);
-
-    // Wait until the background thread finishes
-    background_thread.join().unwrap();
+    println!("Done!");
 }
