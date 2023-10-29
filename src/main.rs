79ba7c72dff162a1e9f360549d20160e159f2f04
Michael Scholten <michael-scholten@hotmail.nl>
2023-10-29 16:01:05 +0100

    Compare-and-Exchange example

--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,33 @@ use std::{
     thread,
 };
 
-fn allocate_new_id() -> u32 {
-    static NEXT_ID: AtomicU32 = AtomicU32::new(0);
-    let id = NEXT_ID.fetch_add(1, Ordering::Relaxed);
-    if id >= 1000 {
-        NEXT_ID.fetch_sub(1, Ordering::Relaxed);
-        panic!("Too many IDs!");
+fn increment(a: &AtomicU32) {
+    let mut current = a.load(Ordering::Relaxed);
+    loop {
+        let new = current + 1;
+        // Compare_exchange could be replaced by compare_exchange_weak for better performance
+        match a.compare_exchange(current, new, Ordering::Relaxed, Ordering::Relaxed) {
+            Ok(_) => return,
+            Err(v) => current = v,
+        }
     }
-    id
 }
 
 fn main() {
-    let mut threads = vec![];
-    for _ in 0..1002 {
-        threads.push(thread::spawn(allocate_new_id));
-        for i in (0..threads.len()).rev() {
-            if threads[i].is_finished() {
-                let thread = threads.remove(i);
-                if let Ok(id) = thread.join() {
-                    println!("{id}");
+    let value = AtomicU32::new(0);
+    thread::scope(|s| {
+        const MAX: usize = 1_000_000;
+        let mut threads = Vec::with_capacity(MAX);
+        let mut max_length = 0;
+        for _ in 0..MAX {
+            threads.push(s.spawn(|| increment(&value)));
+            max_length = max_length.max(threads.len());
+            for i in (0..threads.len()).rev() {
+                if threads[i].is_finished() && threads.remove(i).join().is_ok() {
+                    println!("{}", value.load(Ordering::Relaxed));
                 }
             }
         }
-    }
+        println!("{max_length}");
+    });
 }
