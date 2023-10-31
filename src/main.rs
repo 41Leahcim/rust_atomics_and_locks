04c464302c745066ff1bbc920d604cb5147f2075
Michael Scholten <michael-scholten@hotmail.nl>
2023-10-31 14:55:51 +0100

    Optimized release and acquire ordering example

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
 use std::{
-    sync::atomic::{AtomicBool, AtomicU64, Ordering},
+    sync::atomic::{AtomicBool, Ordering},
     thread,
     time::Duration,
 };
 
-static DATA: AtomicU64 = AtomicU64::new(0);
+/// # Safety
+/// Only one thread is accessing DATA at a time, because it's only writte to
+/// once before and only read after setting the ready flag.
+static mut DATA: u64 = 0;
 static READY: AtomicBool = AtomicBool::new(false);
 
 fn main() {
     thread::spawn(|| {
-        DATA.store(123, Ordering::Relaxed);
+        // Nothing else is accessing DATA, because the READY flag hasn't been set yet
+        unsafe { DATA = 123 };
         READY.store(true, Ordering::Release); // Everything from before this store ..
     });
 
@@ -18,5 +22,7 @@ fn main() {
         thread::sleep(Duration::from_millis(100));
         println!("Waiting...");
     }
-    println!("{}", DATA.load(Ordering::Relaxed));
+
+    // Nothing is mutating DATA, because READY is set
+    println!("{}", unsafe { DATA });
 }
