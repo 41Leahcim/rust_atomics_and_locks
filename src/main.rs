334ca92a79d75f8d35b3d2a0482002b6113d6dc6
Michael Scholten <michael-scholten@hotmail.nl>
2024-06-24 16:17:50 +0200

    Simple fencing examples

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,28 @@
-use core::sync::atomic::{AtomicBool, Ordering};
-use std::{sync::atomic::AtomicPtr, thread};
+use core::sync::atomic::{fence, AtomicU16, AtomicU32, AtomicU8, Ordering};
+use std::thread;
 
-static mut DATA: String = String::new();
-static LOCKED: AtomicBool = AtomicBool::new(false);
+static A: AtomicU8 = AtomicU8::new(0);
+static B: AtomicU16 = AtomicU16::new(0);
+static C: AtomicU32 = AtomicU32::new(0);
 
-fn f() {
-    if !LOCKED.swap(true, Ordering::Acquire) {
-        // Safety: We hold the exclusive lock, so nothing else is accessing DATA.
-        unsafe { DATA.push('!') };
-        LOCKED.store(false, Ordering::Release);
-    }
+fn releasing() {
+    fence(Ordering::Release);
+    A.store(1, Ordering::Relaxed);
+    B.store(2, Ordering::Relaxed);
+    C.store(3, Ordering::Relaxed);
 }
 
-#[derive(Debug)]
-struct Data;
-
-fn generate_data() -> Data {
-    Data
-}
-
-fn get_data() -> &'static Data {
-    static PTR: AtomicPtr<Data> = AtomicPtr::new(core::ptr::null_mut::<Data>());
-    let mut p = PTR.load(Ordering::Acquire);
-    if p.is_null() {
-        p = Box::into_raw(Box::new(generate_data()));
-        if let Err(e) = PTR.compare_exchange(
-            core::ptr::null_mut(),
-            p,
-            Ordering::Release,
-            Ordering::Acquire,
-        ) {
-            // Safety: p comes from box::into_raw right above
-            // and wasn't shared with any other thread.
-            drop(unsafe { Box::from_raw(p) });
-            p = e;
-        }
-    }
-
-    // Safety: p is not null and points to a properly initialized value.
-    unsafe { &*p }
+fn acquiring() {
+    let a = A.load(Ordering::Relaxed);
+    let b = B.load(Ordering::Relaxed);
+    let c = C.load(Ordering::Relaxed);
+    fence(Ordering::Acquire);
+    println!("{a}, {b}, {c}");
 }
 
 fn main() {
     thread::scope(|s| {
-        for _ in 0..100 {
-            s.spawn(f);
-            s.spawn(get_data);
-        }
+        s.spawn(releasing);
+        s.spawn(acquiring);
     });
-    unsafe { println!("{} {}", DATA, DATA.len()) };
-    println!("{:?}", get_data());
 }
