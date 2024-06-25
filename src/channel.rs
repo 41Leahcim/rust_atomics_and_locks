4b4906d23f7bfb7b592bf7b86841a1f8ea81c627
Michael Scholten <michael-scholten@hotmail.nl>
2024-06-25 11:50:31 +0200

    The channel now uses a single byte state machine instead of 2 booleans

--- src/channel.rs
+++ src/channel.rs
@@ -1,13 +1,17 @@
 use std::{
     cell::UnsafeCell,
     mem::MaybeUninit,
-    sync::atomic::{AtomicBool, Ordering},
+    sync::atomic::{AtomicU8, Ordering},
 };
+
+const EMPTY: u8 = 0;
+const WRITING: u8 = 1;
+const READY: u8 = 2;
+const READING: u8 = 3;
 
 pub struct Channel<T> {
     message: UnsafeCell<MaybeUninit<T>>,
-    in_use: AtomicBool,
-    ready: AtomicBool,
+    state: AtomicU8,
 }
 
 unsafe impl<T: Send> Sync for Channel<T> {}
@@ -16,8 +20,7 @@ impl<T> Channel<T> {
     pub const fn new() -> Self {
         Self {
             message: UnsafeCell::new(MaybeUninit::uninit()),
-            in_use: AtomicBool::new(false),
-            ready: AtomicBool::new(false),
+            state: AtomicU8::new(EMPTY),
         }
     }
 
@@ -25,15 +28,17 @@ impl<T> Channel<T> {
     /// When trying to send more than one message.
     pub fn send(&self, message: T) {
         assert!(
-            !self.in_use.swap(true, Ordering::Relaxed),
+            self.state
+                .compare_exchange(EMPTY, WRITING, Ordering::Relaxed, Ordering::Relaxed)
+                .is_ok(),
             "Can't send more than one message!"
         );
         unsafe { (*self.message.get()).write(message) };
-        self.ready.store(true, Ordering::Release);
+        self.state.store(READY, Ordering::Release);
     }
 
     pub fn is_ready(&self) -> bool {
-        self.ready.load(Ordering::Relaxed)
+        self.state.load(Ordering::Relaxed) == READY
     }
 
     /// # Panics
@@ -42,7 +47,9 @@ impl<T> Channel<T> {
     /// Tip: Use `is_ready` to check first.
     pub fn receive(&self) -> T {
         assert!(
-            self.ready.swap(false, Ordering::Acquire),
+            self.state
+                .compare_exchange(READY, READING, Ordering::Acquire, Ordering::Relaxed)
+                .is_ok(),
             "No message available!"
         );
 
@@ -53,7 +60,7 @@ impl<T> Channel<T> {
 
 impl<T> Drop for Channel<T> {
     fn drop(&mut self) {
-        if *self.ready.get_mut() {
+        if *self.state.get_mut() == READY {
             unsafe { self.message.get_mut().assume_init_drop() };
         }
     }
