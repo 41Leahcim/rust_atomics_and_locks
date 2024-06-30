6c9f45e32c0788d9b9e1867f3855b0c9e57d15f0
Michael Scholten <michael-scholten@hotmail.nl>
2024-06-30 07:46:33 +0200

    Blackboxed atomic performance, single loading thread

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,17 @@
-fn main() {}
+use std::{
+    hint::black_box,
+    sync::atomic::{AtomicU64, Ordering},
+    time::Instant,
+};
+
+static A: AtomicU64 = AtomicU64::new(0);
+
+/// Single thread relaxed loading: 0.219901429
+fn main() {
+    black_box(&A);
+    let start = Instant::now();
+    for _ in 0..1_000_000_000 {
+        black_box(A.load(Ordering::Relaxed));
+    }
+    println!("{:?}", start.elapsed().as_secs_f64());
+}
